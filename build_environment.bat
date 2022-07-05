df078406eb26ff07c685432ab8d91ad8df59315e
Christos Athanasiadis <ca@mausbrand.de>
2022-07-05 13:07:21 +0200

    testing build_environment.bat

--- build_environment.bat
+++ build_environment.bat
@@ -1,14 +1,14 @@
 rem [32m Building Docker container and importing to WSL [0m
-docker build --build-arg USER=%USERNAME% -t dev-env -f resources\Dockerfile .
-docker run --name dev-env dev-env
-docker export --output="dev-env.tar.gz" dev-env
-docker container rm dev-env
-docker rmi dev-env
+docker build --build-arg USER=%USERNAME% -t viur-env -f resources\Dockerfile .
+docker run --name viur-env viur-env
+docker export --output="viur-env.tar.gz" viur-env
+docker container rm viur-env
+docker rmi viur-env
 
 rem [32m Setting the WSL version to 2 and importing the new distro [0m
 wsl --set-default-version 2
-wsl --import dev-env .\installDir .\dev-env.tar.gz
-wsl --set-default dev-env
+wsl --import viur-env .\installDir .\viur-env.tar.gz
+wsl --set-default viur-env
 
 rem [32m Cleaning up build files [0m
-del dev-env.tar.gz
+del viur-env.tar.gz
